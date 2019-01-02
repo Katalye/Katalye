b80e43c2cecbf7d722f5b67a1c5776c44555fd2d
Mark Lopez <m@silvenga.com>
2019-01-01 22:54:30 -0600

    Added endpoint to list minions.

---
+++ src/Katalye.Api/Controllers/MinionsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Katalye.Components.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Katalye.Api.Controllers
+{
+    [Route("api/v1/minions")]
+    public class MinionsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public MinionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMinions(GetMinions.Query query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Katalye.Components/Queries/GetMinions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Katalye.Data;
+using MediatR;
+
+namespace Katalye.Components.Queries
+{
+    public static class GetMinions
+    {
+        public class Query : IRequest<Result>, IPaginatedQuery
+        {
+            public int Page { get; set; }
+            public int Size { get; set; }
+        }
+
+        public class Result : PagedResult<Minion>
+        {
+        }
+
+        public class Minion
+        {
+            public string Id { get; set; }
+
+            public DateTimeOffset? LastAuthenticated { get; set; }
+        }
+
+        [UsedImplicitly]
+        public class Handler : IRequestHandler<Query, Result>
+        {
+            private readonly KatalyeContext _context;
+
+            public Handler(KatalyeContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Result> Handle(Query message, CancellationToken cancellationToken)
+            {
+                var result = await _context.Minions
+                                           .Select(x => new Minion
+                                           {
+                                               Id = x.MinionSlug,
+                                               LastAuthenticated = x.LastAuthentication
+                                           })
+                                           .PageAsync(message, new Result());
+
+                return result;
+            }
+        }
+    }
+}
