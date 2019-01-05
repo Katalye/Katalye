a90064476f631958f6f1df9c34dce4a86e79e547
Mark Lopez <m@silvenga.com>
2019-01-05 16:07:20 -0600

    Added return data normalization helper.

---
+++ src/Katalye.Components/Common/JsonFlattener.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Katalye.Components.Common
+{
+    public class JsonFlattener
+    {
+        public IDictionary<string, IList<string>> Flatten(JToken token)
+        {
+            var result = Transverse(token)
+                         .ToLookup(x => x.Key, x => x.Value)
+                         .ToDictionary(x => x.Key, g => (IList<string>) g.ToList());
+
+            return result;
+        }
+
+        private IEnumerable<KeyValuePair<string, string>> Transverse(JToken root)
+        {
+            switch (root.Type)
+            {
+                case JTokenType.Object:
+                case JTokenType.Property:
+                case JTokenType.Array:
+                    foreach (var child in root.Children())
+                    {
+                        foreach (var result in Transverse(child))
+                        {
+                            yield return result;
+                        }
+                    }
+
+                    break;
+                case JTokenType.Integer:
+                case JTokenType.Float:
+                case JTokenType.String:
+                case JTokenType.Boolean:
+                case JTokenType.Null:
+                case JTokenType.Undefined:
+                case JTokenType.Date:
+                case JTokenType.Raw:
+                case JTokenType.Bytes:
+                case JTokenType.Guid:
+                case JTokenType.Uri:
+                case JTokenType.TimeSpan:
+                    yield return new KeyValuePair<string, string>(
+                        root.Parent.Path, root.ToString()
+                    );
+                    break;
+                case JTokenType.None:
+                case JTokenType.Comment:
+                case JTokenType.Constructor:
+                    throw new NotImplementedException($"Traversing {root.Type} is not supported.");
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ tests/Katalye.Components.Tests/GrainDataFacts.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using Katalye.Components.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Katalye.Components.Tests
+{
+    public class GrainDataFacts
+    {
+        private const string GrainFixture =
+            "{\"kernel\":\"Linux\",\"domain\":\"\",\"uid\":0,\"pythonpath\":[\"/usr/bin\"],\"ip_interfaces\":{\"br-950befb17084\":[\"172.18.0.1\",\"fe80::42:6ff:fe06:4490\"]},\"saltversioninfo\":[2018,3,3,0],\"zfs_support\":false,\"SSDs\":[]}";
+
+        [Fact]
+        public void Supports_booleans()
+        {
+            var flattener = new JsonFlattener();
+            var fixture = JsonConvert.DeserializeObject<JToken>(GrainFixture);
+
+            // Act
+            var result = flattener.Flatten(fixture);
+
+            // Assert
+            result.Should().ContainKey("zfs_support")
+                  .WhichValue.Should().ContainSingle(x => x == false.ToString());
+        }
+
+        [Fact]
+        public void Supports_numbers()
+        {
+            var flattener = new JsonFlattener();
+            var fixture = JsonConvert.DeserializeObject<JToken>(GrainFixture);
+
+            // Act
+            var result = flattener.Flatten(fixture);
+
+            // Assert
+            result.Should().ContainKey("uid")
+                  .WhichValue.Should().ContainSingle(x => x == 0.ToString());
+        }
+
+        [Fact]
+        public void Supports_arrays()
+        {
+            var flattener = new JsonFlattener();
+            var fixture = JsonConvert.DeserializeObject<JToken>(GrainFixture);
+
+            // Act
+            var result = flattener.Flatten(fixture);
+
+            // Assert
+            result.Should().ContainKey("pythonpath")
+                  .WhichValue.Should().ContainSingle(x => x == "/usr/bin");
+        }
+
+        [Fact]
+        public void Supports_empty_arrays()
+        {
+            var flattener = new JsonFlattener();
+            var fixture = JsonConvert.DeserializeObject<JToken>(GrainFixture);
+
+            // Act
+            var result = flattener.Flatten(fixture);
+
+            // Assert
+            result.Should().NotContainKey("SSDs");
+        }
+
+        [Fact]
+        public void Supports_nesting()
+        {
+            var flattener = new JsonFlattener();
+            var fixture = JsonConvert.DeserializeObject<JToken>(GrainFixture);
+
+            // Act
+            var result = flattener.Flatten(fixture);
+
+            // Assert
+            result.Should().ContainKey("ip_interfaces.br-950befb17084")
+                  .WhichValue.Should().HaveCount(2)
+                  .And.Subject.Should().Contain(x => x == "172.18.0.1");
+        }
+    }
+}
