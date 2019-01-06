8e686c0d9238e5237323827f69eeeaba0f76df94
Mark Lopez <m@silvenga.com>
2019-01-05 18:04:38 -0600

    Added task to cleanup old grain generations.

---
+++ src/Katalye.Components/Commands/CleanupGrainGenerations.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Hangfire;
+using JetBrains.Annotations;
+using Katalye.Data;
+using Katalye.Data.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NLog;
+
+namespace Katalye.Components.Commands
+{
+    public static class CleanupGrainGenerations
+    {
+        public class Command : IRequest<Result>
+        {
+        }
+
+        public class Result
+        {
+        }
+
+        [UsedImplicitly]
+        public class Handler : IRequestHandler<Command, Result>
+        {
+            private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+            private readonly KatalyeContext _context;
+            private readonly IBackgroundJobClient _jobClient;
+
+            public Handler(KatalyeContext context, IBackgroundJobClient jobClient)
+            {
+                _context = context;
+                _jobClient = jobClient;
+            }
+
+            public Task<Result> Handle(Command message, CancellationToken cancellationToken)
+            {
+                _jobClient.Enqueue<Handler>(x => x.Process());
+
+                return Task.FromResult(new Result());
+            }
+
+            [UsedImplicitly]
+            public async Task Process()
+            {
+                Logger.Info("Processing cleanup of grain generations.");
+
+                using (var unit = await _context.Database.BeginTransactionAsync())
+                {
+                    var grainIds = await (from grain in _context.MinionGrains
+                                          from minion in _context.Minions.Where(x => x.GrainGeneration == grain.Generation).DefaultIfEmpty()
+                                          where minion == null
+                                          select grain.Id).ToListAsync();
+
+                    var grains = grainIds.Select(x => new MinionGrain
+                    {
+                        Id = x
+                    });
+                    _context.MinionGrains.RemoveRange(grains);
+                    await _context.SaveChangesAsync();
+
+                    unit.Commit();
+
+                    Logger.Info($"Removed {grainIds.Count} grains from old generations.");
+                }
+            }
+        }
+    }
+}
---
+++ src/Katalye.Components/Processing/CleanupProcessingServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Katalye.Components.Commands;
+using MediatR;
+
+namespace Katalye.Components.Processing
+{
+    [UsedImplicitly]
+    public class CleanupProcessingServer : ProcessingServer
+    {
+        private readonly IMediator _mediator;
+
+        public override TimeSpan Interval { get; } = TimeSpan.FromHours(1);
+
+        public CleanupProcessingServer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override async Task Process(CancellationToken cancellationToken)
+        {
+            await _mediator.Send(new CleanupGrainGenerations.Command(), cancellationToken);
+        }
+    }
+}
