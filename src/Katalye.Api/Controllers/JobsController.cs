87a52df090d7f03211f72d66120179dd2572cfed
Mark Lopez <m@silvenga.com>
2019-01-19 22:55:12 -0600

    Added JobsController.

---
+++ src/Katalye.Api/Controllers/JobsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Katalye.Api.Controllers
+{
+    [Route("api/v1/jobs")]
+    public class JobsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public JobsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
