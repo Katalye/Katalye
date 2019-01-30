92e34bfa6c36b8f745d177e646719c1728129152
Mark Lopez <m@silvenga.com>
2019-01-30 12:03:18 -0700

    Added support for distributed locking on processing servers.

--- src/Katalye.Components/Processing/ProcessingServer.cs
+++ src/Katalye.Components/Processing/ProcessingServer.cs
@@ -13,33 +13,76 @@ namespace Katalye.Components.Processing
 
         public abstract TimeSpan Interval { get; }
 
+        public virtual bool RequiresDistributedLock => true;
+        public virtual string Name => GetType().Name;
+
         public abstract Task Process(CancellationToken cancellationToken);
 
         public void Execute(BackgroundProcessContext context)
         {
-            Logger.Info($"Beginning execution of processing server {GetType()} with a execution interval of {Interval}.");
+            Logger.Info($"Beginning execution of processing server {Name} with a execution interval of {Interval}.");
 
-            while (!context.IsShutdownRequested)
+            var rock = AcquireDistributedLock(context);
+            try
             {
-                try
+                while (!context.IsShutdownRequested)
                 {
-                    Logger.Info("Processing execution in-progress.");
-
-                    var stopwatch = new Stopwatch();
-                    stopwatch.Start();
-                    Process(context.CancellationToken).GetAwaiter().GetResult();
-                    stopwatch.Stop();
-
-                    Logger.Info($"Processing executed successfully in {stopwatch.ElapsedMilliseconds}ms.");
-
-                    Logger.Info($"Processing server will now sleep for {Interval}.");
-                    context.Wait(Interval);
+                    Process(context);
                 }
-                catch (Exception e)
+            }
+            finally
+            {
+                rock?.Dispose();
+            }
+        }
+
+        private IDisposable AcquireDistributedLock(BackgroundProcessContext context)
+        {
+            if (!RequiresDistributedLock)
+            {
+                Logger.Debug("Distibuted lock is not reqired, none will be aquired.");
+                return null;
+            }
+
+            Logger.Info("Attemping to aquire distibuted lock.");
+            var connection = context.Storage.GetConnection();
+            var rock = connection.AcquireDistributedLock(Name, TimeSpan.FromHours(24));
+            Logger.Info("Distibuted lock achived.");
+            return rock;
+        }
+
+        private void Process(BackgroundProcessContext context)
+        {
+            try
+            {
+                Logger.Info("Processing execution in-progress.");
+
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                Process(context.CancellationToken).GetAwaiter().GetResult();
+                stopwatch.Stop();
+
+                Logger.Info($"Processing executed successfully in {stopwatch.ElapsedMilliseconds}ms.");
+
+                var localInterval = Interval;
+                Logger.Info($"Processing {Name} server will now sleep for {localInterval}.");
+
+                var endTime = DateTimeOffset.Now;
+                while (DateTimeOffset.Now - endTime < localInterval)
                 {
-                    Logger.Warn(e, "An error occured while processing, but was handled by the processing server. The processing server will restart.");
-                    throw;
+                    if (localInterval != Interval)
+                    {
+                        localInterval = Interval;
+                        Logger.Info($"Processing server {Name} interval was changed to {localInterval}.");
+                    }
+
+                    context.Wait(TimeSpan.FromSeconds(5));
                 }
+            }
+            catch (Exception e)
+            {
+                Logger.Warn(e, "An error occured while processing, but was handled by the processing server. The processing server will restart.");
+                throw;
             }
         }
     }
