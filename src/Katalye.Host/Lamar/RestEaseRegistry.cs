4d7bb8f9765ad72159d5282ccecfc86cfbb5da28
Mark Lopez <m@silvenga.com>
2019-02-01 17:35:46 -0600

    Added support for dynamic salt api base uri.

--- src/Katalye.Host/Lamar/RestEaseRegistry.cs
+++ src/Katalye.Host/Lamar/RestEaseRegistry.cs
@@ -1,5 +1,8 @@
-﻿using Katalye.Components;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Katalye.Components.Common;
+using Katalye.Components.Configuration;
 using Lamar;
 using RestEase;
 
@@ -13,9 +16,28 @@ namespace Katalye.Host.Lamar
                                  {
                                      var configuration = x.GetInstance<IKatalyeConfiguration>();
                                      var saltMasterServer = configuration.SaltApiServer;
-                                     return RestClient.For<ISaltApiClient>(saltMasterServer);
+                                     return RestClient.For<ISaltApiClient>(saltMasterServer,
+                                         (request, token) => RequestModifier(request, configuration));
                                  })
                                  .Singleton();
+        }
+
+        private static Task RequestModifier(HttpRequestMessage request, IKatalyeConfiguration configuration)
+        {
+            var scheme = configuration.SaltApiServer.Scheme;
+            var host = configuration.SaltApiServer.Host;
+            var port = configuration.SaltApiServer.Port;
+
+            var builder = new UriBuilder(request.RequestUri)
+            {
+                Scheme = scheme,
+                Host = host,
+                Port = port
+            };
+
+            request.RequestUri = builder.Uri;
+
+            return Task.CompletedTask;
         }
     }
 }
