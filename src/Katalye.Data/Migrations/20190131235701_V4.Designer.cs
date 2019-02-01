8c40f9b4c54933984125694a628e9abb19461c14
Mark Lopez <m@silvenga.com>
2019-01-31 18:39:53 -0600

    Added migration around server settings.

--- src/Katalye.Data/Migrations/20190131235701_V4.Designer.cs
+++ src/Katalye.Data/Migrations/20190131235701_V4.Designer.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using Katalye.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Katalye.Data.Migrations
 {
     [DbContext(typeof(KatalyeContext))]
-    partial class KatalyeContextModelSnapshot : ModelSnapshot
+    [Migration("20190131235701_V4")]
+    partial class V4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Katalye.Data/Migrations/20190131235701_V4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Katalye.Data.Migrations
+{
+    public partial class V4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServerConfigurationValues",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Key = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    LastUpdated = table.Column<DateTimeOffset>(nullable: true),
+                    CreatedOn = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerConfigurationValues", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServerConfigurationValues");
+        }
+    }
+}
