333de5bc58423bc95eb21cc6bb714639773b4217
Mark Lopez <m@silvenga.com>
2019-02-01 21:16:55 -0600

    Handled case were salt api uri is not set.

---
+++ src/Katalye.Components/Exceptions/InvalidConfigurationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Katalye.Components.Exceptions
+{
+    [Serializable]
+    public class InvalidConfigurationException : Exception
+    {
+        public InvalidConfigurationException(string message) : base(message)
+        {
+        }
+
+        public InvalidConfigurationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected InvalidConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- src/Katalye.Host/Lamar/RestEaseRegistry.cs
+++ src/Katalye.Host/Lamar/RestEaseRegistry.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Katalye.Components.Common;
 using Katalye.Components.Configuration;
+using Katalye.Components.Exceptions;
 using Lamar;
 using RestEase;
 
@@ -15,15 +16,22 @@ namespace Katalye.Host.Lamar
             For<ISaltApiClient>().Use(x =>
                                  {
                                      var configuration = x.GetInstance<IKatalyeConfiguration>();
-                                     var saltMasterServer = configuration.SaltApiServer;
+                                     var saltMasterServer = new Uri("http://localhost:8000");
                                      return RestClient.For<ISaltApiClient>(saltMasterServer,
-                                         (request, token) => RequestModifier(request, configuration));
+                                         (request, token) => RequestModifier(request, configuration)
+                                     );
                                  })
                                  .Singleton();
         }
 
         private static Task RequestModifier(HttpRequestMessage request, IKatalyeConfiguration configuration)
         {
+            var server = configuration.SaltApiServer;
+            if (server == null)
+            {
+                throw new InvalidConfigurationException("No Salt API server is configured.");
+            }
+
             var scheme = configuration.SaltApiServer.Scheme;
             var host = configuration.SaltApiServer.Host;
             var port = configuration.SaltApiServer.Port;
