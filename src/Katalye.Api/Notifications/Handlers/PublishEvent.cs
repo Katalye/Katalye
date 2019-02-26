9ef1873d92305efa994a580d39a91c436a323d03
Mark Lopez <m@silvenga.com>
2019-02-25 22:48:03 -0600

    Added notification around events.

---
+++ src/Katalye.Api/Notifications/Handlers/PublishEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Katalye.Api.Hubs;
+using Katalye.Components.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using NLog;
+
+namespace Katalye.Api.Notifications.Handlers
+{
+    [UsedImplicitly]
+    public class PublishEvent : INotificationHandler<EventOccured.Notification>
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly IHubContext<EventsHub> _hubContext;
+
+        public PublishEvent(IHubContext<EventsHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task Handle(EventOccured.Notification notification, CancellationToken cancellationToken)
+        {
+            Logger.Info($"Publishing notification {notification.Path}.");
+            await _hubContext.Clients.All.SendAsync("publish", notification.Path, notification.Data, cancellationToken);
+        }
+    }
+}
---
+++ src/Katalye.Components/Notifications/EventOccured.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Katalye.Components.Notifications
+{
+    public static class EventOccured
+    {
+        public class Notification : INotification
+        {
+            public string Path { get; set; }
+
+            public IDictionary<string, string> Data { get; set; }
+        }
+    }
+}
